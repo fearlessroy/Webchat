870555ffb2e92547335d24ae3a770a8366db89de
yunfeiroy666 <yunfeiroy@gmail.com>
2017-03-29 09:54:17 +0800

    resolve confilct

---
+++ src/main/java/com/wyf/WebMessageApplication.java
@@ -0,0 +1,12 @@
+package com.wyf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebMessageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebMessageApplication.class, args);
+	}
+}
